5c39a45b042f9d7519e3bf5937a5ee083a6dde63
Neil Enns <{ID}+{username}@users.noreply.github.com>
2021-11-13 16:54:38 -0800

    Add ABM back in with an ifdef

--- src/is31fl3733.cpp
+++ src/is31fl3733.cpp
@@ -308,3 +308,86 @@ void IS31FL3733::SetPWM(uint8_t *values)
   // Write LED PWM values to device registers.
   WritePagedRegs(IS31FL3733_LEDPWM, values, IS31FL3733_SW * IS31FL3733_CS);
 }
+
+#ifdef ENABLE_ABM
+void IS31FL3733::SetLEDMode(uint8_t cs, uint8_t sw, IS31FL3733_LED_MODE mode)
+{
+  uint8_t offset;
+
+  // Check SW boundaries.
+  if (sw < IS31FL3733_SW)
+  {
+    // Check CS boundaries.
+    if (cs < IS31FL3733_CS)
+    {
+      // Set mode of individual LED.
+      // Calculate LED offset.
+      offset = sw * IS31FL3733_CS + cs;
+      // Write LED mode to device register.
+      WritePagedReg(IS31FL3733_LEDABM + offset, mode);
+    }
+    else
+    {
+      // Set mode of full row selected by SW.
+      for (cs = 0; cs < IS31FL3733_CS; cs++)
+      {
+        // Calculate LED offset.
+        offset = sw * IS31FL3733_CS + cs;
+        // Write LED mode to device register.
+        WritePagedReg(IS31FL3733_LEDABM + offset, mode);
+      }
+    }
+  }
+  else
+  {
+    // Check CS boundaries.
+    if (cs < IS31FL3733_CS)
+    {
+      // Set mode of full column selected by CS.
+      for (sw = 0; sw < IS31FL3733_SW; sw++)
+      {
+        // Calculate LED offset.
+        offset = sw * IS31FL3733_CS + cs;
+        // Write LED mode to device register.
+        WritePagedReg(IS31FL3733_LEDABM + offset, mode);
+      }
+    }
+    else
+    {
+      // Set mode of all LEDs.
+      for (sw = 0; sw < IS31FL3733_SW; sw++)
+      {
+        for (cs = 0; cs < IS31FL3733_CS; cs++)
+        {
+          // Calculate LED offset.
+          offset = sw * IS31FL3733_CS + cs;
+          // Write LED mode to device register.
+          WritePagedReg(IS31FL3733_LEDABM + offset, mode);
+        }
+      }
+    }
+  }
+}
+
+void IS31FL3733::ConfigABM(IS31FL3733_ABM_NUM n, IS31FL3733_ABM *config)
+{
+  // Set fade in and fade out time.
+  WritePagedReg(n, config->T1 | config->T2);
+  // Set hold and off time.
+  WritePagedReg(n + 1, config->T3 | config->T4);
+  // Set loop begin/end time and high part of loop times.
+  WritePagedReg(n + 2, config->Tend | config->Tbegin | ((config->Times >> 8) & 0x0F));
+  // Set low part of loop times.
+  WritePagedReg(n + 3, config->Times & 0xFF);
+}
+
+void IS31FL3733::StartABM()
+{
+  // Clear B_EN bit in configuration register.
+  WritePagedReg(IS31FL3733_CR, IS31FL3733_CR_SSD);
+  // Set B_EN bit in configuration register.
+  WritePagedReg(IS31FL3733_CR, IS31FL3733_CR_BEN | IS31FL3733_CR_SSD);
+  // Write 0x00 to Time Update Register to update ABM settings.
+  WritePagedReg(IS31FL3733_TUR, 0x00);
+}
+#endif
